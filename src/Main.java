89602bdfb4c975832c48398a92ff479168c8a525
Ghanem <gsa5157@psu.edu>
2023-01-26 12:07:16 -0500

    Added the objects of gold, sword, mace and arrow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
     Sword sword1 = new Sword();
+    Gold gold1 = new Gold(5);
+    Arrow arrow1 = new Arrow();
+    Pillow pillow1 = new Pillow();
+    Mace mace1 = new Mace();
+
     }
 }
