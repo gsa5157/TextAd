53fdbd55b4b3339e445927f6cd0afbad82aee072
Ghanem <gsa5157@psu.edu>
2023-01-31 10:55:52 -0500

    LM2 Enemy Classes

---
+++ src/Creeper.java
@@ -0,0 +1,5 @@
+public class Creeper extends Enemy{
+    public Creeper(){
+        super("Creeper", 35, 20);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
     Arrow arrow = new Arrow();
     Pillow softPillow = new Pillow();
     Mace bigMace = new Mace();
-
+    Ogre greenOgre = new Ogre();
+    Ogre redOgre = new Ogre();
+    Zombie slowZombie = new Zombie();
+    Zombie fastZombie = new Zombie();
+    Dog spotDog = new Dog();
+    Creeper slowCreeper = new Creeper();
     }
 }
